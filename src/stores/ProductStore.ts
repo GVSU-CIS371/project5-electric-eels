dc7a0ab40a591efe724c7a4ae79eb919a96aaad9
zeratini <gritterb@mail.gvsu.edu>
2024-04-03 18:49:58 +0000

    Added Pinia store with relevant actions

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -4,4 +4,22 @@ import { initProducts } from "../data-init";
 
 export const useProductStore = defineStore("ProductStore", {
   // your answer
+  //initProducts is the list of items
+  //ProductDoc is the type of each item
+
+
+  state: () => {
+      return { items: [] as ProductDoc[] };
+    },
+    actions: {
+      init() {
+        this.items = initProducts;
+      },
+      filterByCategory(category: string) {
+        return this.items.filter((item) => item.data.category === category);
+      },
+      filterByRating(minRating: number) {
+        return this.items.filter((item) => item.data.rating >= minRating);
+      }
+    },
 });
