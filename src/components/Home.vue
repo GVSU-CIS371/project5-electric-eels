3e199d66af2deb3d7f202ed531d427cd0497eb6c
zeratini <gritterb@mail.gvsu.edu>
2024-04-11 23:41:44 +0000

    So far I've narrowed the issue to the v-for in Home.vue. it seems like if it errors, the error scales up and nullifies its vuetify containers.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-row> 
-    <v-col>
-      <template v-for="prod in store.items" :key="prod">
-        <StoreItem :prod = "prod"></StoreItem>
-      </template>
-    </v-col>
-  </v-row>
+  <span>a</span>
+  <v-container>
+    <span>b</span>
+    <v-row align="center"> 
+      <span>c</span>
+      <v-col>
+        <span>d</span>
+        <!--<template v-for="(item) in store.items" :key="item" :item = "item">
+          <span>this works</span>
+          <StoreItem :prod = "item"></StoreItem>
+          <span>this works</span>
+        </template>
+        -->
+        <span>e</span>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
@@ -13,6 +23,6 @@
   import { useProductStore } from '../stores/ProductStore';
 
   const store = useProductStore();
+  console.log(store.items);
 
-  // your answer
 </script>
