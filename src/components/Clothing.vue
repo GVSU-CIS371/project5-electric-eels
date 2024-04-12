dbbfa514ab5cad74c7d7d3b858397ac4998aa64a
JenkiJ <jaden.j44@hotmail.com>
2024-04-12 14:21:02 -0400

    Updated style for all pages

--- src/components/Clothing.vue
+++ src/components/Clothing.vue
@@ -1,17 +1,15 @@
 <template>
   <v-container>
     <v-row> 
-      <v-col>
-        <template v-for="(item) in clothing" :key="item" :item = "item">
-          <StoreItem :prod = "item"></StoreItem>
-        </template>
+      <v-col cols="12" sm="6" md="4" v-for="item in clothing" :key="item.id">
+        <StoreItem :prod = "item"></StoreItem>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-  //import StoreItem from './StoreItem.vue';
+  import StoreItem from './StoreItem.vue';
   import { useProductStore } from '../stores/ProductStore';
 
   const store = useProductStore();
