862b5ae21bae5de22fcb45eccd8b5cd4ecdcee60
zeratini <gritterb@mail.gvsu.edu>
2024-04-12 18:17:04 +0000

    Fixed comment in clothing

--- src/components/Clothing.vue
+++ src/components/Clothing.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts" setup>
-  //import StoreItem from './StoreItem.vue';
+  import StoreItem from './StoreItem.vue';
   import { useProductStore } from '../stores/ProductStore';
 
   const store = useProductStore();
